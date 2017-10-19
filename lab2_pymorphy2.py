164f58cb75ac00b500dda3d8957a5069f3e8a778
mike-vankov <dreamer94mv@gmail.com>
2017-10-19 12:23:48 +0500

    Исправлено 3 раз

--- lab2_pymorphy2.py
+++ lab2_pymorphy2.py
@@ -4,16 +4,14 @@ import pymorphy2
 with open("warANDpeace.txt", "rb") as f: 
 	sourceText = f.read().decode('utf8')
 
-MISTRUTH_DEGREE = 0.5 # степень недоверия библиотеке pymorphy2 :)
+MISTRUTH_DEGREE = 0.4 # степень недоверия библиотеке pymorphy2 :)
 
 words = re.findall(r'[ёа-яА-Я-]{3,}', sourceText)
 
 morph = pymorphy2.MorphAnalyzer()
-geoSet = set()
 
 for word in words:
 	parse = morph.parse(word)
 	for p in parse:
 		if ('Geox' in p.tag) and (p.score > MISTRUTH_DEGREE):
-			geoSet.add(p.normal_form)
-print(geoSet)
+			print(p.normal_form)
--- lab2_pymystem.py
+++ lab2_pymystem.py
@@ -1,19 +1,11 @@
-import re
 from pymystem3 import Mystem
-
-with open("warANDpeaceSmall.txt", "rb") as f: 
-	sourceText = f.read().decode('utf8')
-
-words = re.findall(r'[ёа-яА-Я-]{3,}', sourceText)
 mystem = Mystem()
-geoSet = set()
-
-for word in words:
-	analysis = mystem.analyze(word)[0].get('analysis')		# метод analyze() возвращает список из 2 элементов: 1. разбор самого слова, 2. разбор символа '\n' (откуда берется '\n' ??)
-
-	gr = analysis[0].get('gr')								# значение по ключу 'analysis' содержит список из одного элемента, в котором словарь с ключами 'gr' и 'lex')
-	normalForm = analysis[0].get('lex')
-	if 'гео' in gr:
-		geoSet.add(normalForm)
-
-print(geoSet)
+with open("warANDpeace.txt", "rb") as f: 
+    sourceText = f.read().decode('utf8')   
+for token in mystem.analyze(sourceText): 
+    try:       
+        for tokenAnalysis in token['analysis']:
+            if('гео' in tokenAnalysis['gr']):
+                print(tokenAnalysis['lex'])
+    except KeyError:        # Возникает в спецсимволах. (token['analysis'] отсутствует)
+    	pass
